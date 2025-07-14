818915af5430221ddbc74c841e86d2f5e963896d
HighLevelWorking <highlevelworking@gmail.com>
2025-07-15 04:39:23 +0500

    Update main.py
    Image signature done...

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import os
 
 image = None
 
+
 def image_chooser():
     global image
     image = open(r"C:\Users\highl\OneDrive\Pictures\Screenshots\Screenshot 2025-07-14 201528.png", "rb")
+    image = image.read(8)
+
+
+def signature_checking():
+    expected_signature = b'\x89PNG\r\n\x1a\n'
+    if image != expected_signature:
+        print("Invalid image signature. Not a correct png file.")
+    else:
+        print("Image signature is valid. Proceeding with the image.")
 
 def image_opener():
     os.system("explorer C:\\Users\\highl\\OneDrive\\Pictures\\Screenshots\\Screenshot 2025-07-14 201528.png")
@@ -15,9 +25,9 @@ def convert_to_jpg():
 
 def main():
     image_chooser()
-    image_opener()
-    print("Image opened successfully.")
-    print(image.read())
+    signature_checking()
+    #image_opener()
+    #print("Image opened successfully.")
 
 
 if __name__ == "__main__":
